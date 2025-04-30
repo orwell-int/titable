73464efca7f28f0ddd76b7ff531add4645f481fc
dchilot <dchilot@hotmail.com>
2025-05-01 00:51:40 +0200

    Fix upload script

--- upload.py
+++ upload.py
@@ -89,7 +89,7 @@ def main():
     hashes = get_remote_hashes(port, args.baud)
     print(hashes)
     for file in sources.glob("*.py"):
-        if file in ("file_mock.py",) or file.startswith("test_"):
+        if file in ("file_mock.py",) or file.name.startswith("test_"):
             continue
         print(file)
         do_upload = True
